7d3c58f1ed3b4eee3892a59c8b6030b27773927f
Erica <chae_osu@yahoo.com>
2015-04-20 15:45:03 -0700

    fix the delete method. page functions accordingly.

--- js/RestController.js
+++ js/RestController.js
@@ -2,13 +2,19 @@
 //calling the method on studentRoster app, creates a controller
 //function holds the controller's logic
 bestRestaurant.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
-        $scope.restaurants = [];
-        $scope.addRestaurant = function() {
-            $scope.restaurants.push({ name: $scope.restName, type: $scope.restType, location: $scope.restLocation, price: $scope.restPrice });
-            $scope.restName = null;
-            $scope.restType = null;
-            $scope.restLocation = null;
-            $scope.restPrice = null;
-        };
+    $scope.restaurants = [];
+    $scope.addRestaurant = function() {
+        $scope.restaurants.push({ name: $scope.restName, type: $scope.restType, location: $scope.restLocation, price: $scope.restPrice });
+        $scope.restName = null;
+        $scope.restType = null;
+        $scope.restLocation = null;
+        $scope.restPrice = null;
+    };
+    $scope.deleteRestaurant = function(restaurant) {
+        var index = $scope.restaurants.indexOf(restaurant);
+        $scope.restaurants.splice(index,1);
+    };
+
+
 
 });
