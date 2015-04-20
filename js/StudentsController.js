29246950555a68c3fcf009f99c6b0210284f0ea3
Erica <chae_osu@yahoo.com>
2015-04-20 11:31:21 -0700

    this will add students by the user.

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -2,12 +2,10 @@
 //calling the method on studentRoster app, creates a controller
 //function holds the controller's logic
 studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
-    $scope.students = [
-        { name: "Sam Schmidt" },
-        { name: "Jessica Martin"},
-        { name: "Sandy Smith" },
-        { name: "Ryan Samuels"},
-        { name: "Brentwood Davis"}
-
-    ]
+    $scope.students = [];
+    $scope.addStudent = function() {
+        //push the name entered into the list of students
+        $scope.students.push({ name: $scope.studentName });
+        $scope.studentName = null;
+    };
 });
