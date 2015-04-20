1efeccfd789cb54ed7a8859ceaff1c3e457b1b61
Erica <chae_osu@yahoo.com>
2015-04-20 15:10:36 -0700

    add form for user to enter restaurant.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 //first argument is the name of our app
 //second is any dependies that the app might have, will leave empty since app has none
 //module is a container for different parts of the app
+var bestRestaurant = angular.module('bestRestaurant',[]);
--- js/RestController.js
+++ js/RestController.js
@@ -1,3 +1,14 @@
 //create first controller and populare some date to have students to list
 //calling the method on studentRoster app, creates a controller
 //function holds the controller's logic
+bestRestaurant.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+        $scope.restaurants = [];
+        $scope.addRestaurant = function() {
+            $scope.restaurants.push({ name: $scope.restName, type: $scope.restType, location: $scope.restLocation, price: $scope.restPrice });
+            $scope.restName = null;
+            $scope.restType = null;
+            $scope.restLocation = null;
+            $scope.restPrice = null;
+        };
+
+});
